3ed3b7e1aa7262a625b189bfd2a275513accec34
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2022-10-22 14:42:19 -0400

    final

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Gallery from "./components/Gallery";
+import Buttons from "./components/Buttons";
+
+function App (){
+let [artId, setArtId] = useState(12720)
+let [data, setData] = useState({})
+
+const handleIterate = (e) => {
+  setArtId(artId + Number(e.target.value))
+}
+
+useEffect(() => {
+    document.title='Welcome to Artworld'
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+    .then(response => response.json())
+    .then(resData => setData(resData))
+}, [artId])
+
+  return (
+    <div>
+      <Gallery 
+      artist={data.artistDisplayName}
+      objectImg={data.primaryImageSmall}
+      />
+      <Buttons handleIterate={handleIterate}/>
+    </div>
+  );
+}
+
+export default App;
